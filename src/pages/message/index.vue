d289da200e64c064a86aaa8fc9067863781e1f11
YQ Z <qigege13.jc@gmail.com>
2019-02-15 20:50:02 +0800

    消息页

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,12 +1,55 @@
 <template>
   <div id="message">
-    <h1>message</h1>
+    <van-nav-bar title="消息"/>
+
+    <ul>
+      <li class="border-bottom">消息列表
+        <van-icon name="arrow"/>
+      </li>
+      <li class="border-topbottom">好友
+        <van-icon name="arrow"/>
+      </li>
+      <li class="border-topbottom">好友名片
+        <van-icon name="arrow"/>
+      </li>
+      <li class="border-top">关注列表
+        <van-icon name="arrow"/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import Vue from "vue";
+import { NavBar } from "vant";
+Vue.use(NavBar);
+export default {
+  methods: {}
+};
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss" >
+#message {
+  .van-nav-bar {
+    background: #f1457c;
+    .van-nav-bar__title {
+      color: white;
+    }
+  }
+  ul {
+    padding-top: 30px;
+    width: 100%;
+    li {
+      width: 100%;
+      background: white;
+      height: 80px;
+      line-height: 80px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 32px;
+      padding: 0 20px;
+      box-sizing: border-box;
+    }
+  }
+}
 </style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -7,6 +7,6 @@
 <script>
 export default {};
 </script>
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 
 </style>
